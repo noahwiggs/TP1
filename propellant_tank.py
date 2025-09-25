e2902915de606886039614dd3c226db3f30b07f7
fryuzhang <frzhang@terpmail.umd.edu>
2025-09-25 12:14:23 -0400

    finishing doc string for finding fuel and ox volume

--- propellant_tank.py
+++ propellant_tank.py
@@ -12,5 +12,11 @@ def fuel_ox_mix(propellant_mass, fuel, oxidizer):
     fuel_volume (foat): volume of fuel
     oxidizer_mass (float): mass of oxidizer
     oxodizer_volume (float): volume of oxidizer
+
+    --------------------
+    Code takes propellant mass, uses the mixture ratio (weight) to find
+    the mass of the fuel and oxidizer.
+    Then find the volume of the fuel and oxidizer using its density
+
     """
     pass
