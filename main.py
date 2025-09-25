29f5dda20caa5a231210c4b93419c776020b5110
Noah Wigglesworth <nwiggles@umd.edu>
2025-09-25 12:14:13 -0400

    finished pt2 mass estimation

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import dictionaries as dict
 def main():
     """
     General workflow:
-    1) Feed in X for each prop mix
+    1) Feed in X for each prop mix in a for loop
     2) Estimate required masses for heuristics
     3) Calculate heuristics
     4) Produce mass budget for each
 
     """
     #Assumed or fixed vehicle properties
+    X = 0.1 #
+    prop1 = 1
+    prop2 = 1
 
-    X = 0.1
-    
-    = me.mass_estimation(X)
+    m_pr_1, m_pr_2, m_0 = me.mass_estimation(X, prop1, prop2)
+
 
 if __name__ == '__main__':
     main()
