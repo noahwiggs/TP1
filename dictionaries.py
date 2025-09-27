eb09908d6366d363341cbf079216973169d40d02
fryuzhang <frzhang@terpmail.umd.edu>
2025-09-27 14:08:07 -0400

    added sully's dictionary

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+#Dictionaries=(Isp (sec), Trust Stage 1 (N), Thrust Stage 2 (N), Exhaust Area Stage 1 (m), Exhaust Area Stage 2 (m), 
+# Chamber Pressure Stage 1 (Pa), Chamber Pressure Stage 2 (Pa))
+LOX_CH4=dict(Isp=327, Thrust_st1=2260000, Thrust_st2=745000, A_e1=2.4, A_e2=1.5, Pressure_st1=35160000, Pressure_st2=10100000)
+LOX_LH2=dict(Isp=366, Thrust_st1=1860000, Thrust_st2=99000, A_e1=2.4, A_e2=2.15, Pressure_st1=20640000, Pressure_st2=4200000)
+LOX_RP1=dict(Isp=311, Thrust_st1=1920000, Thrust_st2=61000, A_e1=3.7, A_e2=.92, Pressure_st1=25800000,Pressure_st2=6770000)
+Solid=dict(Isp=269, Thrust_st1=4500000, Thrust_st2=2940000, A_e1=6.6, A_e2=2.34, Pressure_st1=10500000, Pressure_st2=5000000)
+Storeable=dict(Isp=285, Thrust_st1=1750000, Thrust_st2=67000, A_e1=1.5, A_e2=1.13, Pressure_st1=15700000, Pressure_st2=14700000)
